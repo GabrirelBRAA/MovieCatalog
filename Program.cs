02841fcdcbd34fad10db50718cd9c24310225d24
Gabriel <jgbiel29@gmail.com>
2024-08-12 11:03:07 -0300

    Tirei a weather API (criado por geraçao de codigo)

--- Program.cs
+++ Program.cs
@@ -34,7 +34,7 @@ var summaries = new[]
 {
     "Freezing", "Bracing", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Scorching"
 };
-
+/*
 app.MapGet("/weatherforecast", () =>
 {
     var forecast =  Enumerable.Range(1, 5).Select(index =>
@@ -49,6 +49,7 @@ app.MapGet("/weatherforecast", () =>
 })
 .WithName("GetWeatherForecast")
 .WithOpenApi();
+*/
 
 app.Run();
 
